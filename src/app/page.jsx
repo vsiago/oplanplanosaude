7b7e172f6053f27564aa84ff8ff39869ac0d14d6
vsiago <vsiago22@gmail.com>
2024-06-17 16:44:55 -0300

    melhoria

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -40,8 +40,9 @@ export default function Home() {
     </section>
 
 
-    <section>
-      <h2>Saúde Integral</h2>
+    <section className="w-full h-[80%] my-20 container mx-auto flex items-center justify-center flex-col">
+      <h2 className="text-4xl uppercase tracking-wider font-semibold my-20 text-[#404E68]">Saúde Integral</h2>
+      <Image src="/oplan-saude-integral.png" width={1000} height={500} alt="Logo Oplan Saúde" />
     </section>
 </>
   );
