1ec8e48e34ff85503832c9d922018f42af65dde6
vsiago <vsiago22@gmail.com>
2024-08-15 15:37:11 -0300

    teste

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,23 +26,7 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
-
-  useEffect(() => {
-    // Adiciona o script do Google Tag Manager dinamicamente
-    const script = document.createElement("script");
-    script.innerHTML = `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PP6X4NL9');
-    `;
-    document.head.appendChild(script);
-  }, []);
-
-
   return (
     <html lang="pt-BR">
       <Head>
