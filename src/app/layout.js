ac27b60fcc9b7becdd47f69381280b82f7e28ca3
vsiago <vsiago22@gmail.com>
2024-09-17 10:11:12 -0300

    teste conexao Google Tag Manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
@@ -27,6 +28,29 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    // Injetar o Google Tag Manager dinamicamente
+    const script = document.createElement("script");
+    script.id = "google-tag-manager-script";
+    script.innerHTML = `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PP6X4NL9');
+    `;
+    document.head.appendChild(script);
+
+    return () => {
+      // Limpar o script caso o componente seja desmontado
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <html lang="pt-BR">
       <Head>
@@ -59,25 +83,6 @@ export default function RootLayout({ children }) {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
-        />
-
-        {/* Google Tag Manager */}
-        <script
-          id="google-tag-manager-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PP6X4NL9');
-            `,
-          }}
         />
       </Head>
       <body className={inter.className}>
