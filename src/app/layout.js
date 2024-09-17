e34485d0962a7724e7d40bbb88f6bcbe66aed6d3
vsiago <vsiago22@gmail.com>
2024-09-17 10:16:18 -0300

    teste1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,16 @@
+"use client";
+
 import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { metadata } from "./metadata"; // Importar o metadata separado
 import "./globals.css";
 
 // Importando a fonte Inter com estilos limitados para melhorar a performance
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
-// Metadata adicionada com mais detalhes para SEO e redes sociais
-export const metadata = {
-  title: "Oplan Plano de Saúde",
-  description: "A sua melhor escolha em Plano de Saúde.",
-  openGraph: {
-    type: "website",
-    url: "https://oplan.com",
-    title: "Oplan Plano de Saúde",
-    description: "A sua melhor escolha em Plano de Saúde.",
-    images: [
-      {
-        url: "https://planodesaudeoplan.com.br/favicon.ico",
-        width: 800,
-        height: 600,
-        alt: "Oplan",
-      },
-    ],
-    site_name: "Oplan",
-  },
-};
-
 export default function RootLayout({ children }) {
   useEffect(() => {
-    // Injetar o Google Tag Manager dinamicamente
     const script = document.createElement("script");
     script.id = "google-tag-manager-script";
     script.innerHTML = `
@@ -46,7 +27,6 @@ export default function RootLayout({ children }) {
     document.head.appendChild(script);
 
     return () => {
-      // Limpar o script caso o componente seja desmontado
       document.head.removeChild(script);
     };
   }, []);
@@ -54,15 +34,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <Head>
-        {/* Tag title dinâmica */}
         <title>{metadata.title}</title>
-
-        {/* Meta description */}
         <meta name="description" content={metadata.description} />
-
-        {/* Open Graph tags para redes sociais */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://oplan.com" />
+        <meta property="og:url" content={metadata.openGraph.url} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta
@@ -70,23 +45,16 @@ export default function RootLayout({ children }) {
           content="https://planodesaudeoplan.com.br/favicon.ico"
         />
         <meta property="og:site_name" content="Oplan" />
-
-        {/* Adicionando outros meta tags */}
         <meta name="robots" content="index, follow" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Fonte Inter com carregamento otimizado */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PP6X4NL9"
---
+++ src/app/metadata.js
@@ -0,0 +1,21 @@
+// src/app/metadata.js
+export const metadata = {
+    title: "Oplan Plano de Saúde",
+    description: "A sua melhor escolha em Plano de Saúde.",
+    openGraph: {
+      type: "website",
+      url: "https://oplan.com",
+      title: "Oplan Plano de Saúde",
+      description: "A sua melhor escolha em Plano de Saúde.",
+      images: [
+        {
+          url: "https://planodesaudeoplan.com.br/favicon.ico",
+          width: 800,
+          height: 600,
+          alt: "Oplan",
+        },
+      ],
+      site_name: "Oplan",
+    },
+  };
+  
